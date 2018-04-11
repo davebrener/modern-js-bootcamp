eeb04a36153697052d163e3ada14b0a638c5eb13
Dave Brener <davebrener@gmail.com>
2018-04-11 16:01:39 -0400

    Added content to boolean.js

--- basics/boolean.js
+++ basics/boolean.js
@@ -1,1 +1,11 @@
-let isFreezing = false;
+let temp = 31;
+let isFreezing = temp < 32;
+
+console.log(isFreezing);
+
+let age = 42;
+let isChild = age <= 7;
+let isSenior = age >= 65;
+
+console.log(isChild);
+console.log(isSenior);
