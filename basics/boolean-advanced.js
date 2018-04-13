fee4284cc1545dc9ad3326bba973a49f6ccfe620
Dave Brener <davebrener@gmail.com>
2018-04-13 13:50:29 -0400

    added boolean-advanced.js

---
+++ basics/boolean-advanced.js
@@ -0,0 +1,24 @@
+let isAccountLocked = false;
+let userRole = 'admin';
+
+if (isAccountLocked) {
+    console.log('is account locked');
+} else if (userRole === 'admin') {
+    console.log('Welcome, Admin');
+} else {
+    console.log('Welcome');
+} 
+
+
+
+// Challenge
+
+let temp = 45;
+
+if (temp<=32) {
+    console.log('It is freezing outside!');
+} else if (temp === 45) {
+    console.log('It is pretty nice');
+} else {
+    console.log('It is hot outside!');
+}
