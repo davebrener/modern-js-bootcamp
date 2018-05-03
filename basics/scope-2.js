0adfd87186fba9a04b46db176b0b2dd4ba7cca4b
Dave Brener <davebrener@gmail.com>
2018-05-03 16:20:20 -0400

    Added content to scope-2.js

---
+++ basics/scope-2.js
@@ -0,0 +1,19 @@
+// GLobal (name)
+  // Local (name)
+    //Local
+  // Local
+
+let name = 'Andrew'
+
+if (true) {
+    let name = 'Dave'
+
+    if (true) {
+        name = 'Jen'
+        console.log(name)
+    }
+}
+
+if (true) {
+    console.log(name)
+}
