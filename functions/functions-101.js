a46195db4196677686b304fc08c588ab369f4ba0
Dave Brener <davebrener@gmail.com>
2018-05-18 11:30:51 -0400

    modified functions-101 and added undefined-null.js

--- functions/functions-101.js
+++ functions/functions-101.js
@@ -5,3 +5,34 @@ let greetUser = function () {
 }
 
 greetUser()
+
+
+let square = function (num) {
+    let result = num * num
+    return result
+}
+let num = 3;
+let value = square(num)
+square(num)
+console.log(value)
+
+
+// Challenge
+
+// Convert Fahrenheit to Celsius
+
+// Call a couple of times (32 -> 0) (68 -> 20)
+
+//Print the converted values
+
+let convertFahrenheitToCelsius = function (fahrenheit) {
+    let celsius = (fahrenheit - 32) * 5 / 9
+    return celsius
+}
+
+let tempOne = convertFahrenheitToCelsius(32)
+let tempTwo = convertFahrenheitToCelsius(68)
+
+console.log(tempOne)
+console.log(tempTwo)
+
